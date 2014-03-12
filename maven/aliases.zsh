92112bdf31cfb9430a5e12deabbf78fe521adcc1
Collin Peters <cpeters@intouchfollowup.com>
2014-03-11 22:13:57 -0700

    Add maven

---
+++ maven/aliases.zsh
@@ -0,0 +1,3 @@
+alias mci="mvn clean install"
+alias mcist="mvn clean install -DskipTests"
+alias mciflex="mvn --projects :intouchV2-flex-parent,:intouchV3-flex-parent -amd clean install"
---
+++ maven/env.zsh
@@ -0,0 +1,4 @@
+export M2_HOME=/usr/share/maven3
+export MAVEN_HOME=$M2_HOME
+export PATH=$PATH:$MAVEN_HOME/bin
+export MAVEN_OPTS="-Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m"

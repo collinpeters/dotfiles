a44ffc370130fcfbcb3c9e30c7a3caa521c37665
Collin Peters <cpeters@intouchfollowup.com>
2014-03-14 11:33:59 -0700

    Move maven customizations to oh-my-zsh plugin

--- oh-my-zsh/custom/plugins/maven/maven.plugin.zsh
+++ oh-my-zsh/custom/plugins/maven/maven.plugin.zsh
@@ -1,3 +1,9 @@
+# see all the maven oh-my-zsh plugin called 'mvn'
+
+# aliases
 alias mci="mvn clean install"
 alias mcist="mvn clean install -DskipTests"
 alias mciflex="mvn --projects :intouchV2-flex-parent,:intouchV3-flex-parent -amd clean install"
+
+# Override the mvn command with the colorized one.
+alias mvn="mvn-color"

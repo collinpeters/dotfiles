46e266a67c9f77cc4426d11567e250d3f2ddddd6
Collin Peters <collin.peters@gmail.com>
2017-06-25 15:08:47 -0700

    Maven cleanup, move to sdkman

--- oh-my-zsh-custom/plugins/maven/maven.plugin.zsh
+++ oh-my-zsh-custom/plugins/maven/maven.plugin.zsh
@@ -4,12 +4,7 @@
 alias mci="mvn clean install"
 alias mcv="mvn clean verify"
 alias mcist="mvn clean install -DskipTests"
-alias mciflex="mvn --projects :intouchV2-flex-parent,:intouchV3-flex-parent,:intouch-distribution -amd clean install"
-alias mcil='cd ~/code/i/intouch; mvn clean install --projects :intouch-flex-resources,:intouch-distribution; cd -' 
 alias m="mvn clean install"
-alias ms="mvn clean install -DskipTests"
-alias mj="mvn clean install -rf java"
-alias msj="mvn clean install -rf java -DskipTests"
 
-# Override the mvn command with the colorized one.
-alias mvn="mvn-color"
+# sdkman defines mvn, set M2_HOME from that
+export M2_HOME=$MAVEN_HOME

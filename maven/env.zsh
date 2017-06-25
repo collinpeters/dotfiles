d700a657170e6b1fff5bcd360ee690e83096e779
Collin Peters <collin.peters@gmail.com>
2017-06-25 15:04:17 -0700

    Maven - disable perm gen and switch to sdkman

--- maven/env.zsh
+++ maven/env.zsh
@@ -1,4 +1,4 @@
-export M2_HOME=/opt/maven
-export MAVEN_HOME=$M2_HOME
-export PATH=$PATH:$MAVEN_HOME/bin
-export MAVEN_OPTS="-Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m"
+#export M2_HOME=/opt/maven
+#export MAVEN_HOME=$M2_HOME
+#export PATH=$PATH:$MAVEN_HOME/bin
+export MAVEN_OPTS="-Xmx1024m"

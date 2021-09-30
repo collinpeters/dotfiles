851b757c571986f126da6b59a26b2c69f68d678a
Collin Peters <itscollin@pm.me>
2021-09-30 15:30:13 -0700

    Sort plugins

--- nvim/xdg_config/init.lua
+++ nvim/xdg_config/init.lua
@@ -5,17 +5,17 @@ require('core.settings')
 require('core.colorscheme')
 
 -- Plugins
+require('plugin-configs.gitsigns')
 require('plugin-configs.lspsaga')
 require('plugin-configs.lualine')
-require('plugin-configs.nvim-treesitter')
-require('plugin-configs.treesitter-just')
-require('plugin-configs.trouble')
 require('plugin-configs.luasnips')
 require('plugin-configs.nvim-cmp')
 require('plugin-configs.nvim-tree')
+require('plugin-configs.nvim-treesitter')
 require('plugin-configs.telescope')
 require('plugin-configs.telescope-fzf-native')
-require('plugin-configs.gitsigns')
+require('plugin-configs.treesitter-just')
+require('plugin-configs.trouble')
 --require('plugin-configs.navigator')
 
 -- LSP

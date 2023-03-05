ae0a187537de8af799d1b489841c586dd9d9a19c
Collin Peters <itscollin@proton.me>
2023-03-04 23:14:43 -0800

    Polybar updates for mandalay

--- polybar/xdg_config/launch.sh
+++ polybar/xdg_config/launch.sh
@@ -20,16 +20,16 @@ AUTORANDR_CURRENT=$(autorandr --current)
 
 if [ "$HOSTNAME" = "mandalay" ]; then
 	if [[ $CONNECTED = *"DP-0"* ]]; then
-		echo "Launching polybar on mandalay DP-0"
-		$POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-dp0 &
+    echo "Launching polybar on mandalay DP-0 (left)"
+		$POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-left &
 	fi
 	if [[ $CONNECTED = *"DP-2"* ]]; then
-		echo "Launching polybar on mandalay DP-2"
-		$POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-dp2 &
+    echo "Launching polybar on mandalay DP-2 (middle)"
+		$POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-middle &
 	fi
 	if [[ $CONNECTED = *"HDMI-0"* ]]; then
-		echo "Launching polybar on mandalay HDMI-0"
-		$POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-hdmi0 &
+    echo "Launching polybar on mandalay HDMI-0 (right)"
+		$POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-right &
 	fi
 elif [ "$HOSTNAME" = "stratosphere" ]; then
   if [[ $AUTORANDR_CURRENT = "triple-4k" ]]; then

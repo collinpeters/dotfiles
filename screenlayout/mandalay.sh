86645751b004a2e55609a11e8d347e06bf4ebbbf
Collin Peters <itscollin@proton.me>
2023-03-05 13:08:09 -0800

    Mandalay screenlayout updates & autorandr for i3

--- screenlayout/mandalay.sh
+++ screenlayout/mandalay.sh
@@ -3,8 +3,8 @@
 #	--output DP-2.8 --auto --panning 3840x2160+3840+0 --right-of DP-0;
 
 xrandr --dpi 228 \
-	--output DP-2 --mode 3840x2160 --pos 0x0 --panning 3840x2160+0+0 \
-	--output DP-0   --mode 3840x2160 --pos 3840x0 --panning 3840x2160+3840+0 --primary \
+	--output DP-0   --mode 3840x2160 --pos 0x0    --panning 3840x2160+0+0 --primary \
+	--output DP-2   --mode 3840x2160 --pos 3840x0 --panning 3840x2160+3840+0 \
 	--output HDMI-0 --mode 3840x2160 --pos 7680x0
 
-~/.config/polybar/launch.sh
+#~/.config/polybar/launch.sh

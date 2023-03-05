b0818a2fc5cd2ecd9b8e8034d8b48150a02a9f3b
Collin Peters <itscollin@proton.me>
2023-03-04 21:38:47 -0800

    Sleep before picture rotate (for login)

--- i3/xdg_config/scripts/bg-script.sh
+++ i3/xdg_config/scripts/bg-script.sh
@@ -8,13 +8,13 @@ cd `dirname "$0"`
 touch $LOG_FILE
 
 while true; do
+	# let it stay for awhile
+	sleep 1h
+
 	# rotate in next one
 	RESULT=$(./bg-rotor.sh $PICTURES_FOLDER $SEEN_FOLDER)
 	echo ${RESULT} >> ${LOG_FILE}
 
 	# keep the log at 100 lines
 	tail -n 100 ${LOG_FILE} | sponge ${LOG_FILE}
-
-	# let it stay for awhile
-	sleep 1h
 done

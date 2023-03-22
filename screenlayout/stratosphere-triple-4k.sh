42b414559c341ce9a1a5d20df7a2d8344b544c27
Collin Peters <itscollin@proton.me>
2023-03-22 15:39:56 -0700

    Display adjustments

--- screenlayout/stratosphere-triple-4k.sh
+++ screenlayout/stratosphere-triple-4k.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Home dock setup: laptop screen off, three external 4k monitors: DP-2, DP-3, HDMI-1
+# Home dock setup: laptop screen off, three external 4k monitors: DP-2, DP-4, HDMI-1
 
 # turn all displays off first
 xrandr --output DP-4  --off \
@@ -11,6 +11,4 @@ xrandr --output DP-4  --off \
 xrandr --dpi 228 \
   --output HDMI-0 --mode 3840x2160 --pos 0x0    --panning 3840x2160+0+0    --primary \
   --output DP-2   --mode 3840x2160 --pos 3840x0 --panning 3840x2160+3840+0 \
-  --output DP-5   --mode 3840x2160 --pos 7680x0 
-
-#~/.config/polybar/launch.sh
+  --output DP-4   --mode 3840x2160 --pos 7680x0 

2a35db0f908dbadd6848800267f4b8555332fe84
Collin Peters <itscollin@proton.me>
2024-02-14 21:16:19 -0800

    Fix Mandalay polybar launch

--- polybar/xdg_config/launch-mandalay.sh
+++ polybar/xdg_config/launch-mandalay.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ $CONNECTED = *"DP-0"* ]]; then
-  echo "Launching polybar on mandalay DP-0 (left)"
-  $POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-left &
-fi
-
-if [[ $CONNECTED = *"DP-2"* ]]; then
-  echo "Launching polybar on mandalay DP-2 (middle)"
-  $POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-middle &
+if [[ $CONNECTED = *"DP-1"* ]]; then
+  echo "Launching polybar on mandalay DP-1 (left)"
+  $POLYBAR -c /home/collin/.config/polybar/config-mandalay.ini mandalay-left &
 fi
 
 if [[ $CONNECTED = *"HDMI-0"* ]]; then
-  echo "Launching polybar on mandalay HDMI-0 (right)"
-  $POLYBAR -c /home/collin/.config/polybar/config-mandalay-forest.ini mandalay-right &
+  echo "Launching polybar on mandalay HDMI-0 (middle)"
+  $POLYBAR -c /home/collin/.config/polybar/config-mandalay.ini mandalay-middle &
+fi
+
+if [[ $CONNECTED = *"DP-2"* ]]; then
+  echo "Launching polybar on mandalay DP-2 (right)"
+  $POLYBAR -c /home/collin/.config/polybar/config-mandalay.ini mandalay-right &
 fi

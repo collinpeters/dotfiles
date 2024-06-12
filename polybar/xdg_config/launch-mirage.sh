7344669b20fd9d1a23bf14391162dc254291149a
Collin Peters <itscollin@proton.me>
2024-06-11 23:04:47 -0700

    Mirage using eDP1 over eDP-1 (after install of xf86-video-intel)

--- polybar/xdg_config/launch-mirage.sh
+++ polybar/xdg_config/launch-mirage.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-if [[ $CONNECTED = *"eDP-1"* ]]; then
-  echo "Launching polybar on mirage eDP-1"
-  $POLYBAR -c /home/collin/.config/polybar/config-mirage mirage-edp1 &
-fi
+echo "Launching polybar on mirage eDP-1"
+$POLYBAR -c /home/collin/.config/polybar/config-mirage mirage-edp1 &

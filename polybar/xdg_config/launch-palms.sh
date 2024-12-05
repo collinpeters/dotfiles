4cab7b74977a7c81330b1a0d7d53cd70715cc56c
Collin Peters <itscollin@proton.me>
2024-12-05 15:46:21 -0800

    Add polybar for palms

---
+++ polybar/xdg_config/launch-palms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Launching polybar on palms Virtual 1"
+$POLYBAR -c /home/collin/.config/polybar/config-palms.ini Virtual1 &
+
+VIRTUAL2=$(xrandr --query | grep "Virtual2 connected" | wc -l)
+if [ "$VIRTUAL2" -eq "1" ]; then
+  echo "Launching polybar on palms Virtual 2"
+  $POLYBAR -c /home/collin/.config/polybar/config-palms.ini Virtual2 &
+fi
+
+VIRTUAL3=$(xrandr --query | grep "Virtual3 connected" | wc -l)
+if [ "$VIRTUAL3" -eq "1" ]; then
+  echo "Launching polybar on palms Virtual 3"
+  $POLYBAR -c /home/collin/.config/polybar/config-palms.ini Virtual3 &
+fi

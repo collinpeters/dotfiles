6f409d14ab73a33ac2344733758d707937315f56
Collin Peters <itscollin@proton.me>
2025-09-03 14:01:50 -0700

    Be clear that default is leader (for vscode-neovim)

--- nvim/xdg_config/lua/config/options.lua
+++ nvim/xdg_config/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Leader key
+vim.g.mapleader = " "
+
 vim.opt.winbar = "%=%m %f"
 
 --disabled, this fights project.nvim

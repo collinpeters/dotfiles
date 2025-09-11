9a5712b7fa0ce8763439270455a7b30947bf4023
Collin Peters <itscollin@proton.me>
2025-09-11 11:35:14 -0700

    Mandalay polybar config

--- polybar/xdg_config/launch-mandalay.sh
+++ polybar/xdg_config/launch-mandalay.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ $CONNECTED = *"DP-1"* ]]; then
-  echo "Launching polybar on mandalay DP-1 (left)"
+if [[ $CONNECTED = *"HDMI-0"* ]]; then
+  echo "Launching polybar on mandalay HDMI-0 (left)"
   $POLYBAR -c /home/collin/.config/polybar/config-mandalay.ini mandalay-left &
 fi
 
-if [[ $CONNECTED = *"HDMI-0"* ]]; then
-  echo "Launching polybar on mandalay HDMI-0 (middle)"
+if [[ $CONNECTED = *"DP-0"* ]]; then
+  echo "Launching polybar on mandalay DP-0 (middle)"
   $POLYBAR -c /home/collin/.config/polybar/config-mandalay.ini mandalay-middle &
 fi
 
--- polybar/xdg_config/launch.sh
+++ polybar/xdg_config/launch.sh
@@ -4,7 +4,7 @@ HOSTNAME=$(hostname)
 POLYBAR=$(which polybar)
 
 if [ -z $PROFILE ]; then
-	PROFILE=$USER
+  PROFILE=$USER
 fi
 
 echo "Launching polybar for host: $HOSTNAME"
@@ -18,39 +18,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 CONNECTED=$(xrandr | grep " connected " | awk '{ print$1 }')
 
 source "$HOME/.config/polybar/launch-${HOSTNAME}.sh"
-
-# if [ "$HOSTNAME" = "mandalay" ]; then
-# elif [ "$HOSTNAME" = "stratosphere" ]; then
-#AUTORANDR_CURRENT=$(autorandr --current)
-#   if [[ $AUTORANDR_CURRENT = "stratosphere-triple-4k" ]]; then
-#     echo "Launching polybar on stratosphere triple 4k"
-#     $POLYBAR -c /home/collin/.config/polybar/config-stratosphere-triple-4k.ini stratosphere-left &
-#     $POLYBAR -c /home/collin/.config/polybar/config-stratosphere-triple-4k.ini stratosphere-middle &
-#     $POLYBAR -c /home/collin/.config/polybar/config-stratosphere-triple-4k.ini stratosphere-right &
-#   else
-#     echo "Launching polybar on stratosphere eDP-1"
-#     $POLYBAR -c /home/collin/.config/polybar/config-stratosphere-single-1080.ini stratosphere-edp1 &
-#   fi
-# elif [ "$HOSTNAME" = "mirage" ]; then
-# else
-#   if [[ $CONNECTED = *"DP-0"* ]]; then
-#     echo "Launching polybar on DP-0"
-#     $POLYBAR dp0 &
-#   fi 
-#
-#   if [[ $CONNECTED = *"DP-3.8"* ]]; then
-#     echo "Launching polybar on DP-3.8"
-#     $POLYBAR dp38 &
-#   elif [[ $CONNECTED = *"DP-3"* ]]; then
-#     echo "Launching polybar on DP-3"
-#     $POLYBAR dp3 &
-#   fi
-#
-#   if [[ $CONNECTED = *"HDMI-0"* ]]; then
-#     echo "Launching polybar on HDMI-0"
-#     $POLYBAR hdmi0 &
-#   fi
-# fi
-#
 
 echo "Bars launched..."

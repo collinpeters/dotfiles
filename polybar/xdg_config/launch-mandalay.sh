d5e89344f9914edf30cb5b1dcacbe676a65aabab
Collin Peters <itscollin@proton.me>
2025-12-18 16:39:59 -0800

    mandalay monitor config update

--- polybar/xdg_config/launch-mandalay.sh
+++ polybar/xdg_config/launch-mandalay.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ $CONNECTED = *"HDMI-0"* ]]; then
-  echo "Launching polybar on mandalay HDMI-0 (left)"
+if [[ $CONNECTED = *"DP-0"* ]]; then
+  echo "Launching polybar on mandalay DP-1"
   $POLYBAR -c /home/collin/.config/polybar/config-mandalay.ini mandalay-left &
 fi
 
-if [[ $CONNECTED = *"DP-0"* ]]; then
-  echo "Launching polybar on mandalay DP-0 (middle)"
+if [[ $CONNECTED = *"HDMI-0"* ]]; then
+  echo "Launching polybar on mandalay HDMI-0"
   $POLYBAR -c /home/collin/.config/polybar/config-mandalay.ini mandalay-middle &
 fi
 
 if [[ $CONNECTED = *"DP-2"* ]]; then
-  echo "Launching polybar on mandalay DP-2 (right)"
+  echo "Launching polybar on mandalay DP-2"
   $POLYBAR -c /home/collin/.config/polybar/config-mandalay.ini mandalay-right &
 fi
